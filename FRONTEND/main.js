bd983126280ca35253435f7c6cd7b89d15a015df
csanaed <csanilajko@gmail.com>
2025-03-15 11:07:27 +0100

    Refactored frontend code corresponding to backend changes, fixed displaying grid

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -1,22 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function addNewStats() {
-        document.getElementById("submitTransaction").addEventListener("click", async function() {
+    function addNewIncome() {
+        document.getElementById("submitIncome").addEventListener("click", async function() {
             let incomeAmount = document.getElementById("incomeAmount").value;
-            let spentAmount = document.getElementById("spentAmount").value;
             let incomeType = document.getElementById("incomeType").value;
-            let spentType = document.getElementById("spentType").value;
-            let currentSavings = document.getElementById("currentSavings");
-            currentSavings.value = Number(incomeAmount) - Number(spentAmount);
 
-            checkNegative()
-    
             const transaction = {
                 incomeAmount: Number(incomeAmount),
-                spentAmount: Number(spentAmount),
                 incomeType: incomeType,
-                spentType: spentType
             }
-            await fetch("http://localhost:5284/MoneyStats/addStat", {
+            await fetch("http://localhost:5284/MoneyStats/addIncome", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(transaction)
@@ -24,15 +16,64 @@ document.addEventListener("DOMContentLoaded", function() {
             .then(resp => console.log(resp.json()))
             .catch(err => console.log(err))
             await getTotalSavings()
+            clearIncome();
         });
     }
 
-    addNewStats()
+    function addNewSpending() {
+        document.getElementById("submitSpending").addEventListener("click", async function () {
+            let spendingAmount = document.getElementById("spentAmount").value;
+            let spendingType = document.getElementById("spentType").value;
+
+            const transaction = {
+                spendingAmount: Number(spendingAmount),
+                spendingType: spendingType
+            }
+
+            await fetch("http://localhost:5284/MoneyStats/addSpending", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(transaction)
+            })
+            .then(resp => console.log(resp.json()))
+            .then(err => console.log(err))
+            await getTotalSavings()
+            clearSpending()
+        })
+        
+    }
+
+    function updateLabelNames() {
+        document.getElementById("incomeAmount").addEventListener("click", function() {
+            document.getElementById("incomeLabel").innerText = `Income from ${document.getElementById("incomeType").value}:`
+        });
+        document.getElementById("spentAmount").addEventListener("click", function() {
+            document.getElementById("spentLabel").innerText = `Money spent on ${document.getElementById("spentType").value}:`
+        });
+    }
+    updateLabelNames()
+    addNewIncome()
+    addNewSpending()
+    getTotalSavings()
+
+    function clearIncome() {
+        document.getElementById("incomeAmount").value = "";
+    }
+
+    function clearSpending () {
+        document.getElementById("spentAmount").value = "";
+    }
 
     async function getTotalSavings() {
         const response = await fetch("http://localhost:5284/MoneyStats/getSavings");
         const savingResponse = await response.json();
-        document.getElementById("totalSavings").value = savingResponse.totalSavings;
+        const currencyType = document.getElementById("currencyType").value;
+        if (savingResponse.totalSavings === Number(0)) {
+            document.getElementById("totalSavings").value = "";
+        }
+        else {
+            document.getElementById("totalSavings").value = `${savingResponse.totalSavings} ${currencyType}`;
+        }
     }
 
 })
