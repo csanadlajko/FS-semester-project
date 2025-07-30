10b13eba6ec04347189aced2a6de93fd14a33782
csanaed <csanilajko@gmail.com>
2025-07-30 21:09:17 +0200

    FE changes for avergae spending display

--- FRONTEND/js/calc_endpoints.js
+++ FRONTEND/js/calc_endpoints.js
@@ -19,6 +19,44 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    document.getElementById("showAvgSpendingStats").addEventListener("click", async function () {
+        await displayAverageSpending();
+        document.getElementById("closeAvgSpending").addEventListener("click", function() {
+            document.getElementById("spending-average-container").style.display = "none";
+        });
+    })
+
+    async function displayAverageSpending() {
+        document.getElementById("spending-average-container").style.display = "flex";
+        const avgSpendingBody = document.getElementById("spending-avg-form");
+        avgSpendingBody.innerHTML = "";
+
+        const filteredSpending = await getAvgSpending();
+        filteredSpending.forEach(item => {
+            const innerSpendingFormDiv = document.createElement("div");
+            innerSpendingFormDiv.classList.add("mb-3");
+
+            const typeLabel = document.createElement("label");
+            typeLabel.classList.add("col-form-label");
+            typeLabel.htmlFor = `${item.spendingType}-amountField`;
+            typeLabel.innerText = item.spendingType;
+
+            const amountField = document.createElement("input");
+            amountField.classList.add("form-control")
+            amountField.type = "text";
+            amountField.value = item.spendingAmount;
+            amountField.disabled = true;
+            amountField.id = `${item.spendingType}-amountField`;
+
+            innerSpendingFormDiv.appendChild(typeLabel);
+            innerSpendingFormDiv.appendChild(amountField);
+
+            avgSpendingBody.appendChild(innerSpendingFormDiv);
+        });
+
+        createCloseButton("spending-avg-form", "closeAvgSpending");
+    }
+
     async function displayAverageIncome() {
         document.getElementById("income-average-container").style.display = "flex";
         const avgIncomeBody = document.getElementById("income-avg-form");
@@ -38,8 +76,8 @@ document.addEventListener('DOMContentLoaded', function() {
             amountField.type = "text";
             amountField.classList.add("form-control")
             amountField.value = item.incomeAmount;
-            amountField.disabled = true
-            amountField.id = `${item.incomeType}-amountField`
+            amountField.disabled = true;
+            amountField.id = `${item.incomeType}-amountField`;
 
             innerIncomeFormDiv.appendChild(typeLabel);
             innerIncomeFormDiv.appendChild(amountField);
@@ -47,12 +85,22 @@ document.addEventListener('DOMContentLoaded', function() {
             avgIncomeBody.appendChild(innerIncomeFormDiv);
         })
 
+        createCloseButton("income-avg-form", "closeAvgIncome")
+
+    }
+
+    function createCloseButton(parent_id, button_id) {
+        const parent = document.getElementById(parent_id)
+        const closeButtonDiv = document.createElement("div");
+        closeButtonDiv.classList.add("d-grid")
+
         const closeButton = document.createElement("input");
         closeButton.type = "button";
-        closeButton.id = "closeAvgIncome";
+        closeButton.id = button_id;
         closeButton.classList.add("btn", "btn-danger");
         closeButton.value = "Bezárás";
-        avgIncomeBody.appendChild(closeButton);
+        closeButtonDiv.appendChild(closeButton);
+        parent.appendChild(closeButtonDiv);
     }
 
 });
