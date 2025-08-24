cc6cfb36ea0bdeab1db7fadbe7adef501299064d
csanaed <csanilajko@gmail.com>
2025-08-24 10:49:56 +0200

    Added parent predict class

--- BACKEND/KOLTSEGVETESIELEMZO.BACKEND/Predict/Calculation/predict.py
+++ BACKEND/KOLTSEGVETESIELEMZO.BACKEND/Predict/Calculation/predict.py
@@ -3,42 +3,53 @@ from Constants.CommonConstants import CommonConstants
 from sklearn.linear_model import LinearRegression
 from Functions.CommonAPIFunctions import CommonAPIFunctions
 
-class PredictIncome:
+class PredictFunctions:
+    
+    def __init__(self, data: pd.DataFrame, budget_type: str | None = None) -> None:
+        """
+        ``dtype`` and ``amount`` value must be 'income' or 'spending'
+        """
+        self.data = data
+        self.dtype = f"{budget_type}Type"
+        self.amount = f"{budget_type}Amount"
+        
+    def _avgFilteredStat(self) -> pd.DataFrame:
+        avg_filtered_data: pd.DataFrame = self.data.groupby(self.dtype, as_index=False)[self.amount].mean()
+        return avg_filtered_data
+        
+    def _verifyData(self, treshold: float) -> bool:
+        avg_data: pd.DataFrame = self._avgFilteredStat()
+        avg_amount: float = avg_data.loc[avg_data[self.dtype] == avg_data[self.dtype], self.amount].item()
+        upper_treshold: float = (avg_amount * treshold) + avg_amount
+        lower_treshold: float = avg_amount - (avg_amount * treshold)
+        if (avg_data[self.dtype] > lower_treshold and avg_data[self.amount] < upper_treshold):
+            return True
+        return False
+
+class PredictIncome(PredictFunctions):
     ALL_INCOME_API: str = f"{CommonConstants.LOCAL_BASE_URL}{CommonConstants.ALL_INCOME_ENDPOINT}"
     
     def __init__(self) -> None:
         self.income_data: pd.DataFrame = CommonAPIFunctions.parse_money_stats(endpoint=PredictIncome.ALL_INCOME_API, income=True)
         self.treshold = 0.1
+        super().__init__(self.income_data, "income")
     
     def average_income_by_type(self) -> pd.DataFrame:
-        average_price: pd.DataFrame = self.income_data.groupby("incomeType", as_index=False)["incomeAmount"].mean()
-        return average_price
+        return super()._avgFilteredStat()
     
     def check_income(self, income_data: dict[str, float]) -> bool:
-        all_avg_data: pd.DataFrame = self.average_income_by_type()
-        avg_amount: float = all_avg_data.loc[all_avg_data["incomeType"] == income_data["incomeType"], "incomeAmount"].item()
-        upper_treshold: float = (avg_amount * self.treshold) + avg_amount
-        lower_treshold: float = avg_amount - (avg_amount * self.treshold)
-        if (income_data["incomeAmount"] > lower_treshold and income_data["incomeAmount"] < upper_treshold):
-            return True
-        return False
+        return super()._verifyData(self.treshold)
 
-class PredictSpending:
+class PredictSpending(PredictFunctions):
     ALL_SPENDING_API: str = f"{CommonConstants.LOCAL_BASE_URL}{CommonConstants.ALL_SPENDING_ENDPOINT}"
     
     def __init__(self) -> None:
         self.spending_data: pd.DataFrame = CommonAPIFunctions.parse_money_stats(endpoint=PredictSpending.ALL_SPENDING_API, income=False)
         self.treshold = 0.1
-    
+        super().__init__(self.spending_data, "spending")
+
     def average_spending_by_type(self) -> pd.DataFrame:
-        avg_price: pd.DataFrame = self.spending_data.groupby("spendingType", as_index=False)["spendingAmount"].mean()
-        return avg_price
+        return super()._avgFilteredStat()
     
-    def check_income(self, income_data: dict[str, float]) -> bool:
-        all_avg_spending: pd.DataFrame = self.average_spending_by_type()
-        avg_for_type: float = all_avg_spending.loc[all_avg_spending["spendingType"] == income_data["spendingType"], "spendingAmount"].item()
-        upper_treshold: float = avg_for_type + (avg_for_type * self.treshold)
-        lower_treshold: float = avg_for_type - (avg_for_type *self.treshold)
-        if (income_data["spendingAmount"] > lower_treshold and income_data["spendingAmount"] < upper_treshold):
-            return True
-        return False
+    def check_income(self) -> bool:
+        return super()._verifyData(self.treshold)
